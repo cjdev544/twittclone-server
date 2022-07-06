3ad2890dfa9096bc29cb57b34a4590c87c7e24d0
Jefferson Campos <cjdev544@gmail.com>
2022-07-06 16:01:33 -0400

    change test 1  port

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 
 dbConfig()
 
-const PORT = precess.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 
 const startServer = async () => {
   const server = new ApolloServer({
